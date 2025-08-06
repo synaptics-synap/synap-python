0bfda1f15b1030dcbe3a7ff9735db20072800438
spal-synaptics <sauryadeep.pal@synaptics.com>
2025-08-06 23:14:46 +0000

    Update `synap_cli_*` examples

--- examples/synap_cli_ic.py
+++ examples/synap_cli_ic.py
@@ -1,4 +1,3 @@
-import argparse
 import json
 import os
 import time
@@ -9,43 +8,80 @@ from synap.postprocessor import Classifier
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', help='synap model')
-    parser.add_argument('input', help='input image')
-    args = parser.parse_args()
 
-    args.model = args.model or "model.synap"
     if not os.path.exists(args.model):
-        raise FileNotFoundError(f"'{args.model}' not found")
-    
-    with open("/usr/share/synap/models/image_classification/imagenet/info.json") as f:
-        labels = json.load(f)["labels"]
+        raise FileNotFoundError(f"Models file '{args.model}' not found")
+
+    if not os.path.exists(args.labels):
+        print(f"Warning: Labels file '{args.labels}' not found")
+        labels = None
+    else:
+        with open(args.labels) as f:
+            labels = json.load(f)["labels"]
 
     network = Network(args.model)
     preprocessor = Preprocessor()
-    classifier = Classifier(top_count=5)
+    classifier = Classifier(top_count=args.top)
     print("\nNetwork        :", args.model)
-    print("Input          :", args.input)
 
-    time_pre = time.time()
-    preprocessor.assign(network.inputs, args.input)
-    time_pre = 1000 * (time.time() - time_pre)
+    for inp in args.inputs:
+        print("Input image    :", inp)
+        time_pre = time.time()
+        preprocessor.assign(network.inputs, inp)
+        time_pre = 1000 * (time.time() - time_pre)
 
-    time_inf = time.time()
-    outputs = network.predict()
-    time_inf = 1000 * (time.time() - time_inf)
+        time_inf = time.time()
+        outputs = network.predict()
+        time_inf = 1000 * (time.time() - time_inf)
 
-    time_post = time.time()
-    result = classifier.process(outputs)
-    time_post = 1000 * (time.time() - time_post)
+        time_post = time.time()
+        result = classifier.process(outputs)
+        time_post = 1000 * (time.time() - time_post)
 
-    print(f"Detection time : {time_pre + time_inf + time_post:.3f} ms ", end="")
-    print(f"(pre: {1000 * time_pre:.3f} us, inf: {1000 * time_inf:.3f} us, post: {1000 * time_post:.3f} us)\n")
+        print(f"Detection time : {time_pre + time_inf + time_post:.3f} ms ", end="")
+        print(f"(pre: {1000 * time_pre:.3f} us, inf: {1000 * time_inf:.3f} us, post: {1000 * time_post:.3f} us)")
 
-    print("Class  Confidence  Description")
-    for item in result.items:
-        print(f"{item.class_index:5d}{item.confidence:12.4f}  {labels[item.class_index]}")
+        print("Class  Confidence  Description")
+        for item in result.items:
+            print(f"{item.class_index:5d}{item.confidence:12.4f}  ", end="")
+            if labels is not None:
+                print(labels[item.class_index])
+            else:
+                print()
     print()
 
+
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Image classification on image files")
+    parser.add_argument(
+        "-m", "--model",
+        type=str,
+        metavar="<file>",
+        default="model.synap",
+        help="Model file (.synap) (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--top",
+        type=int,
+        metavar="<n>",
+        default=5,
+        help="Number of classification results to show (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--labels",
+        type=str,
+        metavar="<file>",
+        default="/usr/share/synap/models/image_classification/imagenet/info.json",
+        help="Image classification labels (default: imagenet @ '%(default)s')"
+    )
+    parser.add_argument(
+        "inputs",
+        type=str,
+        nargs="+",
+        help="Input image file(s)"
+    )
+    args = parser.parse_args()
+
     main()
--- examples/synap_cli_od.py
+++ examples/synap_cli_od.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 import time
 
@@ -8,43 +7,97 @@ from synap.postprocessor import Detector
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', help='synap model')
-    parser.add_argument('input', help='input image')
-    args = parser.parse_args()
 
-    args.model = args.model or "model.synap"
     if not os.path.exists(args.model):
-        raise FileNotFoundError(f"'{args.model}' not found")
+        raise FileNotFoundError(f"Models file '{args.model}' not found")
 
     network = Network(args.model)
     preprocessor = Preprocessor()
-    detector = Detector()
+    detector = Detector(
+        args.score_threshold,
+        args.max_detections,
+        not args.disable_nms,
+        args.iou_threshold,
+        args.iou_with_min
+    )
     print("\nNetwork        :", args.model)
-    print("Input          :", args.input)
 
-    time_pre = time.time()
-    assigned_rect = preprocessor.assign(network.inputs, args.input)
-    time_pre = 1000 * (time.time() - time_pre)
+    for inp in args.inputs:
+        print("Input image    :", inp)
+        time_pre = time.time()
+        assigned_rect = preprocessor.assign(network.inputs, inp)
+        time_pre = 1000 * (time.time() - time_pre)
 
-    time_inf = time.time()
-    outputs = network.predict()
-    time_inf = 1000 * (time.time() - time_inf)
+        time_inf = time.time()
+        outputs = network.predict()
+        time_inf = 1000 * (time.time() - time_inf)
 
-    time_post = time.time()
-    result = detector.process(outputs, assigned_rect)
-    time_post = 1000 * (time.time() - time_post)
+        time_post = time.time()
+        result = detector.process(outputs, assigned_rect)
+        time_post = 1000 * (time.time() - time_post)
 
-    print(f"Detection time : {time_pre + time_inf + time_post:.3f} ms ", end="")
-    print(f"(pre: {1000 * time_pre:.3f} us, inf: {1000 * time_inf:.3f} us, post: {1000 * time_post:.3f} us)\n")
+        print(f"Detection time : {time_pre + time_inf + time_post:.3f} ms ", end="")
+        print(f"(pre: {1000 * time_pre:.3f} us, inf: {1000 * time_inf:.3f} us, post: {1000 * time_post:.3f} us)")
 
-    print("#   Score  Class   Position        Size  Description     Landmarks")
-    for i, item in enumerate(result.items):
-        bb = item.bounding_box
-        print(f"{i:<3}  {item.confidence:.2f} {item.class_index:>6}  {bb.origin.x:>4},{bb.origin.y:>4}   {bb.size.x:>4},{bb.size.y:>4}  {'':<16}", end="")
-        for lm in item.landmarks:
-            print(f" {lm}", end="")
-        print()
+        print("#   Score  Class   Position        Size  Description     Landmarks")
+        for i, item in enumerate(result.items):
+            bb = item.bounding_box
+            print(f"{i:<3}  {item.confidence:.2f} {item.class_index:>6}  {bb.origin.x:>4},{bb.origin.y:>4}   {bb.size.x:>4},{bb.size.y:>4}  {'':<16}", end="")
+            for lm in item.landmarks:
+                print(f" {lm}", end="")
+            print()
+
 
 if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Object detection on image files")
+    parser.add_argument(
+        "-m", "--model",
+        type=str,
+        metavar="<file>",
+        default="model.synap",
+        help="Model file (.synap) (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--score-threshold",
+        type=float,
+        default=0.5,
+        metavar="<thr>",
+        help="Min confidence (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--max-detections",
+        type=int,
+        metavar="<n>",
+        default=0,
+        help="Max number of detections [0: all] (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--disable-nms",
+        action="store_true",
+        default=False,
+        help="Disable Non-Max-Suppression algorithm"
+    )
+    parser.add_argument(
+        "--iou-threshold",
+        type=float,
+        metavar="<thr>",
+        default=0.5,
+        help="IOU threashold for NMS (default: %(default)s)"
+    )
+    parser.add_argument(
+        "--iou-with-min",
+        action="store_true",
+        default=False,
+        help="Use min area instead of union to compute IOU"
+    )
+    parser.add_argument(
+        "inputs",
+        type=str,
+        nargs="+",
+        help="Input image file(s)"
+    )
+    args = parser.parse_args()
+
     main()
