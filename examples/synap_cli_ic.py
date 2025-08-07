01bdb3077b73294c57495f7abc8740c3100aa099
spal-synaptics <sauryadeep.pal@synaptics.com>
2025-08-07 17:59:13 +0000

    Add JSON export to `synap_cli_*` examples

--- examples/synap_cli_ic.py
+++ examples/synap_cli_ic.py
@@ -1,31 +1,42 @@
 import json
-import os
+import sys
 import time
+from pathlib import Path
 
 from synap import Network
 from synap.preprocessor import Preprocessor
-from synap.postprocessor import Classifier
+from synap.postprocessor import Classifier, to_json_str
 
 
 def main():
 
-    if not os.path.exists(args.model):
+    model_file = Path(args.model)
+    if not model_file.exists():
         raise FileNotFoundError(f"Models file '{args.model}' not found")
 
-    if not os.path.exists(args.labels):
-        print(f"Warning: Labels file '{args.labels}' not found")
-        labels = None
-    else:
-        with open(args.labels) as f:
-            labels = json.load(f)["labels"]
+    labels = None
+    labels_file = Path(args.labels) if args.labels else None
+    if labels_file:
+        if not labels_file.exists():
+            print(f"Warning: Labels file '{args.labels}' not found")
+        else:
+            labels = json.loads(labels_file.read_text())["labels"]
 
-    network = Network(args.model)
+    stdout = sys.stdout.isatty()
+    all_results = {"network": str(model_file.resolve()), "results": []}
+    if labels_file:
+        all_results["labels"] = str(labels_file.resolve())
+
+    network = Network(model_file)
     preprocessor = Preprocessor()
     classifier = Classifier(top_count=args.top)
-    print("\nNetwork        :", args.model)
+    if stdout:
+        print("\nNetwork        :", model_file)
 
     for inp in args.inputs:
-        print("Input image    :", inp)
+        curr_result = {"input": str(Path(inp).resolve())}
+        if stdout:
+            print("Input image    :", inp)
         time_pre = time.time()
         preprocessor.assign(network.inputs, inp)
         time_pre = 1000 * (time.time() - time_pre)
@@ -38,17 +49,30 @@ def main():
         result = classifier.process(outputs)
         time_post = 1000 * (time.time() - time_post)
 
-        print(f"Detection time : {time_pre + time_inf + time_post:.3f} ms ", end="")
-        print(f"(pre: {1000 * time_pre:.3f} us, inf: {1000 * time_inf:.3f} us, post: {1000 * time_post:.3f} us)")
+        curr_result["detection_time_ms"] = {
+            "pre": time_pre,
+            "inf": time_inf,
+            "post": time_post
+        }
+        if stdout:
+            print(f"Detection time : {time_pre + time_inf + time_post:.3f} ms ", end="")
+            print(f"(pre: {1000 * time_pre:.3f} us, inf: {1000 * time_inf:.3f} us, post: {1000 * time_post:.3f} us)")
 
-        print("Class  Confidence  Description")
-        for item in result.items:
-            print(f"{item.class_index:5d}{item.confidence:12.4f}  ", end="")
-            if labels is not None:
-                print(labels[item.class_index])
-            else:
-                print()
-    print()
+        curr_result["result"] = json.loads(to_json_str(result))
+        if stdout:
+            print("Class  Confidence  Description")
+            for item in result.items:
+                print(f"{item.class_index:5d}{item.confidence:12.4f}  ", end="")
+                if labels is not None:
+                    print(labels[item.class_index])
+                else:
+                    print()
+        all_results["results"].append(curr_result)
+
+    if stdout:
+        print()
+    else:
+        print(json.dumps(all_results, indent=2))
 
 
 if __name__ == "__main__":
@@ -74,7 +98,7 @@ if __name__ == "__main__":
         type=str,
         metavar="<file>",
         default="/usr/share/synap/models/image_classification/imagenet/info.json",
-        help="Image classification labels (default: imagenet @ '%(default)s')"
+        help="JSON file containing image classification labels (default: imagenet @ '%(default)s')"
     )
     parser.add_argument(
         "inputs",
--- examples/synap_cli_od.py
+++ examples/synap_cli_od.py
@@ -1,17 +1,34 @@
+import json
 import os
+import sys
 import time
+from pathlib import Path
 
 from synap import Network
 from synap.preprocessor import Preprocessor
-from synap.postprocessor import Detector
+from synap.postprocessor import Detector, to_json_str
 
 
 def main():
 
-    if not os.path.exists(args.model):
+    model_file = Path(args.model)
+    if not model_file.exists():
         raise FileNotFoundError(f"Models file '{args.model}' not found")
 
-    network = Network(args.model)
+    labels = None
+    labels_file = Path(args.labels) if args.labels else None
+    if labels_file:
+        if not labels_file.exists():
+            print(f"Warning: Labels file '{args.labels}' not found")
+        else:
+            labels = json.loads(labels_file.read_text())["labels"]
+
+    stdout = sys.stdout.isatty()
+    all_results = {"network": str(model_file.resolve()), "results": []}
+    if labels_file:
+        all_results["labels"] = str(labels_file.resolve())
+
+    network = Network(model_file)
     preprocessor = Preprocessor()
     detector = Detector(
         args.score_threshold,
@@ -20,10 +37,13 @@ def main():
         args.iou_threshold,
         args.iou_with_min
     )
-    print("\nNetwork        :", args.model)
+    if stdout:
+        print("\nNetwork        :", model_file)
 
     for inp in args.inputs:
-        print("Input image    :", inp)
+        curr_result = {"input": str(Path(inp).resolve())}
+        if stdout:
+            print("Input image    :", inp)
         time_pre = time.time()
         assigned_rect = preprocessor.assign(network.inputs, inp)
         time_pre = 1000 * (time.time() - time_pre)
@@ -36,16 +56,34 @@ def main():
         result = detector.process(outputs, assigned_rect)
         time_post = 1000 * (time.time() - time_post)
 
-        print(f"Detection time : {time_pre + time_inf + time_post:.3f} ms ", end="")
-        print(f"(pre: {1000 * time_pre:.3f} us, inf: {1000 * time_inf:.3f} us, post: {1000 * time_post:.3f} us)")
+        curr_result["detection_time_ms"] = {
+            "pre": time_pre,
+            "inf": time_inf,
+            "post": time_post
+        }
+        if stdout:
+            print(f"Detection time : {time_pre + time_inf + time_post:.3f} ms ", end="")
+            print(f"(pre: {1000 * time_pre:.3f} us, inf: {1000 * time_inf:.3f} us, post: {1000 * time_post:.3f} us)")
 
-        print("#   Score  Class   Position        Size  Description     Landmarks")
-        for i, item in enumerate(result.items):
-            bb = item.bounding_box
-            print(f"{i:<3}  {item.confidence:.2f} {item.class_index:>6}  {bb.origin.x:>4},{bb.origin.y:>4}   {bb.size.x:>4},{bb.size.y:>4}  {'':<16}", end="")
-            for lm in item.landmarks:
-                print(f" {lm}", end="")
-            print()
+        curr_result["result"] = json.loads(to_json_str(result))
+        if stdout:
+            print("#   Score  Class   Position        Size  Description     Landmarks")
+            for i, item in enumerate(result.items):
+                bb = item.bounding_box
+                print(f"{i:<3}  {item.confidence:.2f} {item.class_index:>6}  {bb.origin.x:>4},{bb.origin.y:>4}   {bb.size.x:>4},{bb.size.y:>4}  ", end="")
+                if labels is not None:
+                    print(f"{labels[item.class_index]:<16}")
+                else:
+                    print()
+                for lm in item.landmarks:
+                    print(f" {lm}", end="")
+                print()
+        all_results["results"].append(curr_result)
+
+    if stdout:
+        print()
+    else:
+        print(json.dumps(all_results, indent=2))
 
 
 if __name__ == "__main__":
@@ -91,6 +129,12 @@ if __name__ == "__main__":
         action="store_true",
         default=False,
         help="Use min area instead of union to compute IOU"
+    )
+    parser.add_argument(
+        "--labels",
+        type=str,
+        metavar="<file>",
+        help="JSON file containing object detection labels"
     )
     parser.add_argument(
         "inputs",
