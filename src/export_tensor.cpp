ff60e7a147275063b5d5a2202dd707f2d2d531f1
spal-synaptics <sauryadeep.pal@synaptics.com>
2025-09-19 17:38:15 -0700

    Refactor input checker function to `export_tensor.hpp`

--- src/export_tensor.cpp
+++ src/export_tensor.cpp
@@ -48,54 +48,9 @@ struct TensorsIterator {
 };
 
 static void assign_tensor(Tensor &t, const py::array &data) {
-    const auto &shape = t.shape();
-    const auto &data_dims = data.ndim();
-    const auto &tensor_dims = shape.size();
-    if (data_dims > tensor_dims || data_dims < tensor_dims - 1) {
-        std::ostringstream err;
-        if (data_dims > tensor_dims)
-            err << "Dimensions mismatch: expected " << tensor_dims << " dimensions, got " << data_dims;
-        else
-            err << "Dimensions mismatch: expected " << tensor_dims - 1 << " dimensions, got " << data_dims;
-        throw std::invalid_argument(err.str());
-    }
-
-    bool chw_input = data_dims == tensor_dims - 1;
-    if (chw_input && shape[0] != 1) {
-        std::ostringstream err;
-        err << "Shape mismatch: cannot assign input with shape (";
-        for (size_t i = 0; i < data_dims; i++) {
-            err << data.shape(i) << (i < data_dims - 1 ? ", " : "");
-        }
-        err << ") to tensor with batch dimension > 1";
-        throw std::invalid_argument(err.str());
-    }
-    for (size_t i = 0; i < data_dims; ++i) {
-        size_t shape_idx = chw_input ? i + 1 : i;
-        if (data.shape(i) != shape[shape_idx]) {
-            std::ostringstream err;
-            err << "Shape mismatch: expected (";
-            for (size_t j = chw_input ? 1 : 0; j < shape.size(); ++j) {
-                err << shape[j] << (j < shape.size() - 1 ? ", " : "");
-            }
-            err << "), got (";
-            for (size_t j = 0; j < data_dims; ++j) {
-                err << data.shape(j) << (j < data_dims - 1 ? ", " : "");
-            }
-            err << ")";
-            throw std::invalid_argument(err.str());
-        }
-    }
-
-    const auto &size = t.size();
-    const auto &data_size = data.nbytes();
-    if (data_size != size) {
-        std::ostringstream err;
-        err << "Size mismatch: expected " << size << " bytes, got " << data_size << " bytes";
-        throw std::invalid_argument(err.str());
-    }
-    
+    validate_input(t, data);
     const auto &dtype = data.dtype();
+    const auto &data_size = data.nbytes();
     if (dtype.is(py::dtype::of<uint8_t>())) {
         if (!t.assign(data.unchecked<uint8_t>().data(0), data.size())) {
             throw std::runtime_error("Failed to assign NumPy uint8_t data to tensor");
--- src/include/export_tensor.hpp
+++ src/include/export_tensor.hpp
@@ -6,15 +6,66 @@
 #include <memory>
 #include "synap/tensor.hpp"
 #include "synap/network.hpp"
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
-using namespace synaptics::synap;
+namespace py = pybind11;
 
 struct TensorsWrapper {
-    std::shared_ptr<Network> network;
-    Tensors* tensors;
+    std::shared_ptr<synaptics::synap::Network> network;
+    synaptics::synap::Tensors* tensors;
 };
 
 struct TensorWrapper {
-    std::shared_ptr<Network> network;
-    Tensor* tensor;
+    std::shared_ptr<synaptics::synap::Network> network;
+    synaptics::synap::Tensor* tensor;
 };
+
+inline void validate_input(synaptics::synap::Tensor &t, const py::array &data) {
+    const auto &shape = t.shape();
+    const auto &data_dims = data.ndim();
+    const auto &tensor_dims = shape.size();
+    if (data_dims > tensor_dims || data_dims < tensor_dims - 1) {
+        std::ostringstream err;
+        if (data_dims > tensor_dims)
+            err << "Dimensions mismatch: expected " << tensor_dims << " dimensions, got " << data_dims;
+        else
+            err << "Dimensions mismatch: expected " << tensor_dims - 1 << " dimensions, got " << data_dims;
+        throw std::invalid_argument(err.str());
+    }
+
+    bool chw_input = data_dims == tensor_dims - 1;
+    if (chw_input && shape[0] != 1) {
+        std::ostringstream err;
+        err << "Shape mismatch: cannot assign input with shape (";
+        for (size_t i = 0; i < data_dims; i++) {
+            err << data.shape(i) << (i < data_dims - 1 ? ", " : "");
+        }
+        err << ") to tensor with batch dimension > 1";
+        throw std::invalid_argument(err.str());
+    }
+    for (size_t i = 0; i < data_dims; ++i) {
+        size_t shape_idx = chw_input ? i + 1 : i;
+        if (data.shape(i) != shape[shape_idx]) {
+            std::ostringstream err;
+            err << "Shape mismatch: expected (";
+            for (size_t j = chw_input ? 1 : 0; j < shape.size(); ++j) {
+                err << shape[j] << (j < shape.size() - 1 ? ", " : "");
+            }
+            err << "), got (";
+            for (size_t j = 0; j < data_dims; ++j) {
+                err << data.shape(j) << (j < data_dims - 1 ? ", " : "");
+            }
+            err << ")";
+            throw std::invalid_argument(err.str());
+        }
+    }
+
+    const auto &size = t.size();
+    const auto &data_size = data.nbytes();
+    if (data_size != size) {
+        std::ostringstream err;
+        err << "Size mismatch: expected " << size << " bytes, got " << data_size << " bytes";
+        throw std::invalid_argument(err.str());
+    }
+}
